a97637aad81203417654ff51963c82c986345c9b
stephenwayar <stephenbuluswayar@gmail.com>
2022-05-27 23:50:00 +0100

    setup app with nextjs

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import Head from 'next/head'
+import Nav from '../components/nav/nav'
+
+export default function Home() {
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Eden | Home</title>
+      </Head>
+
+      <Nav />
+    </React.Fragment>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ]
 }
